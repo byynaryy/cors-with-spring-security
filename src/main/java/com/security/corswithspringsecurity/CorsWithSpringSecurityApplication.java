038db08d3ce7fdd2d3eac85a7c33f0cea28f41c0
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-05 23:48:33 +0100

    feat: initial commit

---
+++ src/main/java/com/security/corswithspringsecurity/CorsWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.corswithspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/corswithspringsecurity/CorsWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.corswithspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
